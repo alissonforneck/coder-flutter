36f96f601b72b72cf083de03754a0af87e27068d
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-07-26 17:36:17 -0300

    Create constantes01.dart

---
+++ curso-dart/fundamentos/constantes01.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // Área da circunferencia = PI * raio * raio
+const PI = 3.1415;
+
+stdout.write("Informe o raio: ");
+final entradaDoUsuario = stdin.readLineSync();
+final double raio = double.parse(entradaDoUsuario!);
+
+final area = PI * raio * raio;
+print('O valor da área é: $area');
+}
