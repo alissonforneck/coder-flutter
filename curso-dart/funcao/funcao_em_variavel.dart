254fb2abb79e50a1b833372e19bbd23daa0428cb
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-07-29 17:21:03 -0300

    finalizei a aula de função em variável

--- curso-dart/funcao/funcao_em_variavel.dart
+++ curso-dart/funcao/funcao_em_variavel.dart
@@ -1,8 +1,19 @@
 main() {
-  int resultado = soma(a: 10, b: 20);
-  print(resultado);
+  // criando uma funcao chamada soma 1 que receubeu a funcao somaFn
+  int Function(int, int) soma1 = somaFn;
+  print(soma1(20, 331));
+
+  //crii a funcao soma2 que é uma funcao de inteiro e recebeu a arrow function.
+  int Function(int, int) soma2 = (x, y) => x + y;
+  /*
+    tambem funciona se for feito dessa forma, aqui eu estou fazendo inferindo
+    var soma2 = (x, y) => x + y;
+  */
+
+  print(soma2(7, 10));
+  print(soma2 is Function);
 }
 
-int soma({int a = 1, int b = 1}) {
+int somaFn(int a, int b) {
   return a + b;
 }
