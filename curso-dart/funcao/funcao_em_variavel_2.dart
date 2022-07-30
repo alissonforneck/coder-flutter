6b4b489da22995ff1a268263977041fcf0e473fa
Alisson Forneck <alissonforneckdm@gmail.com>
2022-07-29 22:07:54 -0300

    terminei de criar as funçoes de multiplicacao e divisao

--- curso-dart/funcao/funcao_em_variavel_2.dart
+++ curso-dart/funcao/funcao_em_variavel_2.dart
@@ -1,3 +1,13 @@
 void main(List<String> args) {
-  print('testee');
+  int adicao(int a, int b) {
+    return a + b;
+  }
+
+  int subtracao(int a, int b) => a - b;
+  int multiplicacao(int a, int b) => a * b;
+  double divisao(int a, int b) => a / b;
+  print(adicao(4, 2));
+  print(subtracao(4, 2));
+  print(multiplicacao(4, 2));
+  print(divisao(6, 2));
 }
