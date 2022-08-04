c08e2c96f5cc797561138de982a9bbf630db7c80
Alisson Forneck <alissonforneckdm@gmail.com>
2022-08-04 09:58:52 -0300

    aprendendo mais sobre classes e funcoes

--- curso-dart/oo/testando_conhecimento.dart
+++ curso-dart/oo/testando_conhecimento.dart
@@ -1,24 +1,35 @@
+import 'dart:async';
+
 class Pessoa {
   final nome;
-
+  var dataNascimento = {
+    'dia': 1,
+    'mes': 1,
+    'ano': 1960,
+  };
   Pessoa({
     String this.nome = 'Ze',
   });
-  Map dataNascimento(int d, int m, int a) {
+/*
+  var dataNascimento = (int d, int m, int a) {
     return {
       'dia': d,
       'mes': m,
       'ano': a,
     };
-  }
+  };
+*/
+  //String mostrarDados(this.nome, this.data) {
+  // return 'Meu nome é $nome, e nasci em $dataNascimento';
+  //}
 
   String toString() {
-    return 'Meu nome é $nome, e nasci em ';
+    return 'Meu nome é $nome, e nasci em ${this.dataNascimento}';
   }
 }
 
 void main(List<String> args) {
   var alisson = Pessoa(nome: 'Alison');
-  alisson.dataNascimento(17, 06, 1997);
+  alisson.dataNascimento = {'dia': 17, 'mes': 06, 'ano': 1997};
   print(alisson);
 }
