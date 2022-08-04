e4d63b9e7e6d575f361a2eac9351b17c7030e812
Alisson Forneck <alissonforneckdm@gmail.com>
2022-08-04 09:24:40 -0300

    atualizando a classe pessoa e brincando com o tostring

--- curso-dart/oo/testando_conhecimento.dart
+++ curso-dart/oo/testando_conhecimento.dart
@@ -1,9 +1,24 @@
 class Pessoa {
-  var nome;
+  final nome;
 
-  Pessoa({this.nome = 'Ze'});
+  Pessoa({
+    String this.nome = 'Ze',
+  });
+  Map dataNascimento(int d, int m, int a) {
+    return {
+      'dia': d,
+      'mes': m,
+      'ano': a,
+    };
+  }
+
+  String toString() {
+    return 'Meu nome é $nome, e nasci em ';
+  }
 }
 
 void main(List<String> args) {
   var alisson = Pessoa(nome: 'Alison');
+  alisson.dataNascimento(17, 06, 1997);
+  print(alisson);
 }
