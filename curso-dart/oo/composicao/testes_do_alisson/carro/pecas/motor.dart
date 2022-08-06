3fa8f9dbfb3b8d225ef62485d0d1a94d080d311d
Alisson Forneck <alissonforneckdm@gmail.com>
2022-08-05 21:02:56 -0300

    testei criação de classes e construtores

---
+++ curso-dart/oo/composicao/testes_do_alisson/carro/pecas/motor.dart
@@ -0,0 +1,9 @@
+class Motor {
+  var potencia;
+  var quantidadeDeCavalos;
+  var torque;
+  Motor(
+      {double this.potencia = 0.0,
+      int this.quantidadeDeCavalos = 0,
+      double this.torque = 0.0});
+}
---
+++ curso-dart/oo/composicao/testes_do_alisson/carro/pecas/rodas.dart
@@ -0,0 +1,9 @@
+class Rodas {
+  var pneu;
+  var tamanho;
+  var peso;
+  Rodas(
+      {String this.pneu = '',
+      double this.peso = 0.0,
+      double this.tamanho = 0.0});
+}
